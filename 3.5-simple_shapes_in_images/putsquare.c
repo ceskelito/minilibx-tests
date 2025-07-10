bdf3fd4f9037a7f1e28be8207cde8a832b17e34b
ceske-laptop <ceske@etik.com>
2025-07-10 18:46:31 +0200

    Add colors.h -- Print Square on image

--- 3.5-simple_shapes_in_images/putsquare.c
+++ 3.5-simple_shapes_in_images/putsquare.c
@@ -1,14 +1,39 @@
 #include "mlx.h"
+#include "colors.h"
 #include "imglib.h"
 
+/**
+ * Draws a square outline on the image.
+ *
+ * @param img     Pointer to image data structure.
+ * @param size    Length of the sides of the square (must be >= 2).
+ * @param x       X coordinate of the top-left corner.
+ * @param y       Y coordinate of the top-left corner.
+ * @param color   32-bit integer representing the pixel color.
+ */
 void img_put_square(t_data *img, int size, int x, int y, int color)
 {
+    int i;
 
-	for(int i = 0; i < size; i++)
-	{
-		img_put_pixel(img, x + i , y, color);
-	}
+    if (size < 2)
+        return; // A square must be at least 2x2
+
+    // Top side
+    for (i = 0; i < size; i++)
+        img_put_pixel(img, x + i, y, color);
+
+    // Bottom side
+    for (i = 0; i < size; i++)
+        img_put_pixel(img, x + i, y + size - 1, color);
+
+    // Left and Right sides (excluding corners)
+    for (i = 1; i < size - 1; i++)
+    {
+        img_put_pixel(img, x, y + i, color);               // Left
+        img_put_pixel(img, x + size - 1, y + i, color);     // Right
+    }
 }
+
 
 int	main(void)
 {
@@ -19,13 +44,13 @@ int	main(void)
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 1920, 1080, "Hello, Square!");
 
-	window = (void *)window; // Remove the "set but not used" error
-
 	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(&img.img, &img.bits_per_pixel, &img.line_lenght,
-																&img.endian);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght,
+								&img.endian);
 
-	img_put_square(&img, 10, 100, 100, 0255255255);
+	img_put_square(&img, 200, 100, 100, COLOR_BLUE);
+
+	mlx_put_image_to_window(mlx, window, img.img, 0, 0);
 
 	mlx_loop(mlx);
 }
