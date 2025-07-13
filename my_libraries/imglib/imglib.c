c0239127065ef5f90a86aad42ddd7f8c35a2fc47
ceske-laptop <ceske@etik.com>
2025-07-13 16:39:22 +0200

    renewed lib

--- my_libraries/imglib/imglib.c
+++ my_libraries/imglib/imglib.c
@@ -1,13 +1,13 @@
 #include "imglib.h"
 #include "mlx.h"
 
-int img_get_offset(t_data *img, int x, int y)
+int img_get_offset(t_img *img, int x, int y)
 {
 //	      offset for the line	    nbyte = nbit / 8
 	return (y * img->line_length + x * (img->bits_per_pixel / 8));
 }
 
-void img_put_pixel(t_data *img, int x, int y, int color)
+void img_put_pixel(t_img *img, int x, int y, int color)
 {
 	char	*dest;
 
@@ -15,9 +15,9 @@ void img_put_pixel(t_data *img, int x, int y, int color)
 	*(unsigned int *)dest = color;
 }
 
-void img_init_data(t_data *img, void* mlx, int x_length, int y_length)
+void img_init(t_img *img, void* mlx, int img_length, int img_heigth)
 {
-	img->img = mlx_new_image(mlx, x_length, y_length);
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length,
+	img->img = mlx_new_image(mlx, img_length, img_heigth);
+	img->addr = mlx_get_img_addr(img->img, &img->bits_per_pixel, &img->line_length,
 										&img->endian);
 }
--- my_libraries/imglib/imglib.h
+++ my_libraries/imglib/imglib.h
@@ -1,17 +1,22 @@
 #ifndef IMGLIB_H
 # define IMGLIB_H
 
-typedef struct s_data
-{
+typedef struct s_img {
 	void    *img;
 	char    *addr;                                                            
 	int     bits_per_pixel;
 	int     line_length;
 	int     endian; 
+} t_img;
+
+typedef struct s_data {
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
 } t_data;
 
-int		img_get_offset(t_data *img, int x, int y);
-void	img_put_pixel(t_data *img, int x, int y, int color);
-void	img_init_data(t_data *img, void *mlx, int x_length, int y_length);
+int		img_get_offset(t_img *img, int x, int y);
+void	img_pixel_put(t_img *img, int x, int y, int color);
+void	img_init(t_img *img, void *mlx, int x_length, int y_length);
 
 #endif
