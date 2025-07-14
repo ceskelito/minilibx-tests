ee0bfc9ffca640428d3666ea12ccb8718014cd16
ceske-laptop <ceske@etik.com>
2025-07-14 16:50:55 +0200

    uptated variables.mk

--- variables.mk
+++ variables.mk
@@ -1,5 +1,14 @@
-ROOT := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+ROOT			:= $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+ADDITIVE_LIBS	:= $(ROOT)/my_libraries
+
+# Common Commands
+CC		= gcc
+AR		= ar rcs
+RM		= rm -f
+MKDIR	= mkdir -p
+CFLAGS	= -Wall -Wextra -Werror
 
 # Libraries Directories
-MLXLIB_DIR = $(ROOT)/minilibx-linux
-IMGLIB_DIR = $(ROOT)/imglib
+MLXLIB_DIR		= $(ROOT)/minilibx-linux
+IMGLIB_DIR		= $(ADDITIVE_LIBS)/imglib
+COLORS_DIR		= $(ADDITIVE_LIBS)/colors
