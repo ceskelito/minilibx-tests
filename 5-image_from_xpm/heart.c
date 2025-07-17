583eee560825ea7e9acb75a74fdb2857b5701e1b
ceske-laptop <ceske@etik.com>
2025-07-17 21:57:54 +0200

    Revert "changes to erase"
    This reverts commit 4a3219ae2dc3bcd31ef91bc59b9e772ecb37b93e.

--- 5-image_from_xpm/heart.c
+++ 5-image_from_xpm/heart.c
@@ -1,13 +1,9 @@
 #include "datalib.h"
 #include "mlx.h"
-#include "colors.h"
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
-
-
-void draw_level(t_data *data, t_xpm *red_heart, t_xpm *rev_heart, t_param *p);
 
 typedef struct s_xpm
 {
@@ -33,28 +29,15 @@ int close_window(t_data *data)
     exit(0);
 }
 
-void new_level(t_param *p)
-{
-	int	x;
-	int	y;
-
-	x = WINDOW_LENGTH / 2 - 30;
-	y = WINDOW_HEIGTH / 4;
-	mlx_clear_window(p->data->mlx, p->data->win);
-	mlx_put_image_to_window(p->data->mlx, p->data->win,
-						p->xpm->img, p->x_pos, p->y_pos);
-	mlx_string_put(p->data->mlx, p->data->win, x, y, COLOR_WHITE, "A G A I N");
-	sleep(5);
-	draw_level(p->data, p->red_heart, p->rev_heart, p);
-}
-
 int handle_mouse_click(int button, int x, int y, t_param *p)
 {
 	if(button == 1)
 		if((x >= p->x_pos && x <= p->x_pos + p->xpm->width) &&
 			(y >= p->y_pos && y <= p->y_pos + p->xpm->heigth))
 		{
-			new_level(p);
+			mlx_clear_window(p->data->mlx, p->data->win);
+			mlx_put_image_to_window(p->data->mlx, p->data->win,
+								p->xpm->img, p->x_pos, p->y_pos);
 			return(1);
 		}
 	return(0);
@@ -67,19 +50,6 @@ void set_random_coordinates(int *x, int *y)
 	*y = (rand() % (WINDOW_HEIGTH / 20)) * 20;
 	if(*x > WINDOW_LENGTH || *y > WINDOW_HEIGTH)
 		set_random_coordinates(x, y);
-}
-
-void draw_level(t_data *data, t_xpm *red_heart, t_xpm *rev_heart, t_param *p)
-{
-	set_random_coordinates(&(p->x_pos), &(p->y_pos));
-	for(int i = 20; i < WINDOW_LENGTH - 20; i += 20)
-		for(int j = 20; j < WINDOW_HEIGTH - 20; j += 20)
-		{	
-			if(i == p->x_pos && j == p->y_pos)
-				mlx_put_image_to_window(data->mlx, data->win, rev_heart->img, i, j);
-			else
-				mlx_put_image_to_window(data->mlx, data->win, red_heart->img, i, j);
-		}
 }
 
 int	main(void)
@@ -106,7 +76,14 @@ int	main(void)
 	if(!red_heart.img || !rev_heart.img)
 		return (MLX_ERROR);
 
-	draw_level(&data, &red_heart, &rev_heart, &param);
+	for(int i = 20; i < WINDOW_LENGTH - 20; i += 20)
+		for(int j = 20; j < WINDOW_HEIGTH - 20; j += 20)
+		{	
+			if(i == param.x_pos && j == param.y_pos)
+				mlx_put_image_to_window(data.mlx, data.win, rev_heart.img, i, j);
+			else
+				mlx_put_image_to_window(data.mlx, data.win, red_heart.img, i, j);
+		}
 
 	mlx_hook(data.win, 17, 1L<<17, &close_window, &data);
 	mlx_mouse_hook(data.win, &handle_mouse_click, &param);
